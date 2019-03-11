b54881a85bfe4f1a25d2a317bd9232c6ed875226
zoro <aoping@yougola.com>
2019-03-11 14:16:18 +0800

    init

---
+++ .temp/store.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import user from '@/core/store/modules/user'
+import event from '@/core/store/modules/event'
+import repository from '@/core/store/modules/repository'
+
+
+Vue.use(Vuex)
+
+
+
+export default new Vuex.Store({
+    modules: {
+        user,
+        event,
+        repository
+    },
+    strict: false,
+    plugins:  []
+})
